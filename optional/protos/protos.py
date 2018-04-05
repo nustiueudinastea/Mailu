d23a9d272915985f4677e621719ee0d6e05f5485
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-04-05 15:24:22 +0300

    loading all env variables for all procs. It is easier this way

--- optional/protos/protos.py
+++ optional/protos/protos.py
@@ -7,53 +7,52 @@ from protoslib import protos, exceptions
 from circus import get_arbiter
 from time import sleep
 
-WEBMAIL = 'rainloop'
-WEB_WEBMAIL = '/webmail'
-ADMIN = 'true'
-WEB_ADMIN = '/admin'
-HOSTNAMES = '' # set at runtime
-DOMAIN = 'protos.io'
-TLS_FLAVOR = 'cert'
-RELAYNETS = '172.16.0.0/12'
-MESSAGE_SIZE_LIMIT = '5000000'
-FETCHMAIL_DELAY= '600'
-RECIPIENT_DELIMITER = '+'
-DMARC_RUA = 'admin'
-DMARC_RUF = 'admin'
-WELCOME = 'true'
-WELCOME_SUBJECT = 'Welcome to your new Mailu email account'
-WELCOME_BODY = 'Welcome to your new email account, if you can read this, then Mailu is configured properly and running on your Protos platform!'
+ENV_VARIABLES = {
+    'WEBMAIL': 'rainloop',
+    'WEB_WEBMAIL': '/webmail',
+    'ADMIN': 'true',
+    'WEB_ADMIN': '/admin',
+    'HOSTNAMES': '', # set at runtime
+    'DOMAIN': 'protos.io',
+    'TLS_FLAVOR': 'cert',
+    'RELAYNETS': '172.16.0.0/12',
+    'MESSAGE_SIZE_LIMIT': '5000000',
+    'FETCHMAIL_DELAY': '600',
+    'RECIPIENT_DELIMITER': '+',
+    'DMARC_RUA': 'admin',
+    'DMARC_RUF': 'admin',
+    'WELCOME': 'true',
+    'WELCOME_SUBJECT': 'Welcome to your new Mailu email account',
+    'WELCOME_BODY': 'Welcome to your new email account, if you can read this, then Mailu is configured properly and running on your Protos platform!',
 
-HOST_ADMIN = '127.0.0.1:8080'
-HOST_SMTP = 'smtp:11025'
-HOST_AUTHSMTP = 'smtp:11026'
-HOST_IMAP = 'imap:11143'
-HOST_POP3 = 'imap:11110'
-HOST_WEBMAIL = 'webmail'
+    'HOST_REDIS': '127.0.0.1',
+    'RATELIMIT_STORAGE_URL': 'redis://127.0.0.1',
+    'HOST_ADMIN': '127.0.0.1:8080',
+    'HOST_SMTP': 'smtp:11025',
+    'HOST_AUTHSMTP': 'smtp:11026',
+    'HOST_IMAP': 'imap:11143',
+    'HOST_POP3': 'imap:11110',
+    'HOST_WEBMAIL': '127.0.0.1',
+    'HOST_ANTISPAM': '127.0.0.1:11334',
+    'FRONT_ADDRESS': '127.0.0.1',
+}
 
-PROTOS_URL = 'http://172.17.0.6:9999/'
+PROTOS_URL = 'http://172.17.0.3:9999/'
 PROTOS_APP_ID = 'bb1pq5rj02j1hjk38j0g'
 
+
 def set_proc_vars():
-    rainloop_proc = {'cmd': '/mailu/rainloop/start.sh', 'working_dir': '/var/www', 'numprocesses': 1}
-    rsyslog_proc = {'cmd': '/usr/sbin/rsyslogd -n', 'working_dir': '/', 'numprocesses': 1}
-    redis_proc = {'cmd': '/usr/bin/redis-server', 'working_dir': '/', 'numprocesses': 1}
-    nginx_proc = {'cmd': '/mailu/nginx/start.py', 'working_dir': '/mailu/nginx', 'numprocesses': 1,
-                "env": {'TLS_FLAVOR': TLS_FLAVOR, 'HOSTNAMES': HOSTNAMES, 'ADMIN': ADMIN, 'HOST_ADMIN': HOST_ADMIN,
-                'HOST_SMTP': HOST_SMTP, 'WEB_WEBMAIL': WEB_WEBMAIL, 'WEB_ADMIN': WEB_ADMIN}}
-    mailu_proc = {'cmd': '/mailu/admin/start.sh', 'working_dir': '/mailu/admin' ,'numprocesses': 1,
-                'env': {'DOMAIN': DOMAIN, 'HOST_SMTP': HOST_SMTP, 'HOST_IMAP': HOST_IMAP, 'HOST_POP3': HOST_POP3,
-                'HOST_AUTHSMTP': HOST_AUTHSMTP, 'PROTOS_URL': PROTOS_URL, 'APPID': PROTOS_APP_ID, 'HOSTNAMES': HOSTNAMES,
-                'DMARC_RUA': DMARC_RUA, 'DMARC_RUF': DMARC_RUF}}
-    dovecot_proc = {'cmd': '/mailu/dovecot/start.py', 'working_dir': '/mailu/dovecot' ,'numprocesses': 1,
-                    'env': {'DOMAIN': DOMAIN, 'HOSTNAMES': HOSTNAMES, 'WEBMAIL': WEBMAIL, 'PATH': os.environ['PATH']}}
-    postfix_proc = {'cmd': '/mailu/postfix/start.py', 'working_dir': '/mailu/postfix' ,'numprocesses': 1,
-                    'env': {'DOMAIN': DOMAIN, 'HOSTNAMES': HOSTNAMES, 'RELAYNETS': RELAYNETS,
-                            'MESSAGE_SIZE_LIMIT': MESSAGE_SIZE_LIMIT, 'FETCHMAIL_DELAY': FETCHMAIL_DELAY,
-                            'RECIPIENT_DELIMITER': RECIPIENT_DELIMITER, 'DMARC_RUA': DMARC_RUA, 'DMARC_RUF': DMARC_RUF,
-                            'WELCOME': WELCOME, 'WELCOME_SUBJECT': WELCOME_SUBJECT, 'WELCOME_BODY': WELCOME_BODY}}
-    all_processes = [rainloop_proc, rsyslog_proc, nginx_proc, redis_proc, mailu_proc, dovecot_proc, postfix_proc]
-    # all_processes = [nginx_proc]
+    rainloop_proc = {'name': 'rainloop', 'cmd': '/mailu/rainloop/start.sh', 'working_dir': '/var/www', 'numprocesses': 1}
+    rsyslog_proc = {'name': 'rsyslog', 'cmd': '/usr/sbin/rsyslogd -n', 'working_dir': '/', 'numprocesses': 1}
+    redis_proc = {'name': 'redis', 'cmd': '/usr/bin/redis-server', 'working_dir': '/', 'numprocesses': 1}
+    nginx_proc = {'name': 'nginx', 'cmd': '/mailu/nginx/start.py', 'working_dir': '/mailu/nginx', 'numprocesses': 1, 'env': ENV_VARIABLES}
+    mailu_proc = {'name': 'admin', 'cmd': '/mailu/admin/start.sh', 'working_dir': '/mailu/admin' ,'numprocesses': 1, 'env': ENV_VARIABLES}
+    dovecot_proc = {'name': 'dovecot', 'cmd': '/mailu/dovecot/start.py', 'working_dir': '/mailu/dovecot' ,'numprocesses': 1, 'env': ENV_VARIABLES}
+    postfix_proc = {'name': 'postfix', 'cmd': '/mailu/postfix/start.py', 'working_dir': '/mailu/postfix' ,'numprocesses': 1, 'env': ENV_VARIABLES}
+    rspamd_proc = {'name': 'rspamd', 'cmd': '/mailu/rspamd/start.py', 'working_dir': '/mailu/rspamd' ,'numprocesses': 1, 'env': ENV_VARIABLES}
+
+    # all_processes = [rainloop_proc, rsyslog_proc, nginx_proc, redis_proc, mailu_proc, dovecot_proc, postfix_proc, rspamd_proc]
+    all_processes = [rsyslog_proc, nginx_proc, redis_proc, mailu_proc, postfix_proc, dovecot_proc, rainloop_proc]
     return all_processes
 
 def do_circus(event_loop=None):
@@ -81,8 +80,8 @@ def do_protos():
     app_info = client.get_app_info()
     print(domain)
     print(app_info)
-    global HOSTNAMES
-    HOSTNAMES = app_info['name'] + '.' + domain
+    global ENV_VARIABLES
+    ENV_VARIABLES['HOSTNAMES'] = app_info['name'] + '.' + domain
 
     # #client.delete_resource('b78b0f0c0c3115097f02e0b049336217')
 
